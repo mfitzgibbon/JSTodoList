2e22e644761a5f378d45d4380a6c0a81a92605fe
Michael Fitzgibbon <fitzgibbonm@my.lanecc.edu>
2018-04-16 18:34:01 -0700

    Testing git commit

--- src/scripts.js
+++ src/scripts.js
@@ -87,26 +87,44 @@
 */
 
 class ToDoList{
-    function ToDoList{
-        
+
+    constructor () {
+        this.tasks = [
+            {task: 'Go to Dentist', isComplete: false},
+            {task: 'Do Gardening', isComplete: true},
+            {task: 'Renew Library Account', isComplete: false},
+        ];
+
+        this.LoadTasks();
+
+    }
+
+    loadTasks(){
+        let tasksHtml = this.tasks.reduce(
+            (html, task, index) += this.generateTaskHtml(task, index),
+            '');
+        document.getElementById("taskList").innerHTML = tasksHtml;
+    }
+
+    generateTaskHtml(task, index) {
+        return `
+          <li class="list-group-item checkbox">
+            <div class="row">
+              <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
+                <label><input id="toggleTaskStatus" type="checkbox" value="" class="" ${(task.isComplete)?"checked":"" }></label>
+              </div>
+              <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${(task.isComplete)?"complete":""}>
+                ${task.task}
+              </div>
+              <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
+                <a class="" href="/" ><i id="deleteTask" class="delete-icon glyphicon glyphicon-trash"></i></a>
+              </div>
+            </div>
+          </li>
+        `;
     }
 }
 
-function generateTaskHtml(task, index) {
-    return `
-      <li class="list-group-item checkbox">
-        <div class="row">
-          <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
-            <label><input id="toggleTaskStatus" type="checkbox" value="" class="" IS IT CHECKED></label>
-          </div>
-          <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text SHOULD IT HAVE THE CHECKED CLASS">
-            TASK GOES HERE
-          </div>
-          <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
-            <a class="" href="/" ><i id="deleteTask" class="delete-icon glyphicon glyphicon-trash"></i></a>
-          </div>
-        </div>
-      </li>
-    `;
-}
+let toDo;
+window.onload = () => { toDo = new ToDoList() }
 
