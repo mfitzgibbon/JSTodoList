5e6ebddedf247fb7c028b63469f221ebf5f75d1f
Michael Fitzgibbon <fitzgibbonm@my.lanecc.edu>
2018-04-16 21:46:58 -0700

    Starting work on part 5

--- src/scripts.js
+++ src/scripts.js
@@ -15,7 +15,7 @@
             It gets called in loadTasks
         -   Replace TASK GOES HERE with with a template string that
             will write the task property of the task parameter
-        -   Replace the other 2 ALL caps phrases with template strings
+        -   Rps phrases with tempeplace the other 2 ALL calate strings
             that use the isComplete property of the task
 
     -   Add the loadTasks method
@@ -86,6 +86,14 @@
     Use and arrow or anonymous function
 */
 
+
+
+/*
+window.addEventListener(
+    "load", () => {toDo = new ToDoList();
+});
+*/
+
 class ToDoList{
 
     constructor () {
@@ -95,15 +103,16 @@ class ToDoList{
             {task: 'Renew Library Account', isComplete: false},
         ];
 
-        this.LoadTasks();
+        this.loadTasks();
+        this.addEventListeners();
 
     }
 
     loadTasks(){
         let tasksHtml = this.tasks.reduce(
-            (html, task, index) += this.generateTaskHtml(task, index),
+            (html, task, index) => html += this.generateTaskHtml(task, index),
             '');
-        document.getElementById("taskList").innerHTML = tasksHtml;
+        document.getElementById('taskList').innerHTML = tasksHtml;
     }
 
     generateTaskHtml(task, index) {
@@ -111,20 +120,62 @@ class ToDoList{
           <li class="list-group-item checkbox">
             <div class="row">
               <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
-                <label><input id="toggleTaskStatus" type="checkbox" value="" class="" ${(task.isComplete)?"checked":"" }></label>
+                <label><input id="toggleTaskStatus" type="checkbox" 
+                onchange="toDo.toggleTaskStatus(${index})"
+                value="" class="" ${(task.isComplete)?'checked':''}></label>
               </div>
-              <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${(task.isComplete)?"complete":""}>
+              <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${(task.isComplete)?'complete':''}">
                 ${task.task}
               </div>
               <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
-                <a class="" href="/" ><i id="deleteTask" class="delete-icon glyphicon glyphicon-trash"></i></a>
+                <a class="" href="/" ><i id="deleteTask" class="delete-icon glyphicon glyphicon-trash"
+                onClick = "toDo.deleteTask(event, ${index})"></i></a>
               </div>
             </div>
           </li>
         `;
     }
+
+    toggleTaskStatus(index) {
+        this.tasks[index].isComplete = !this.tasks[index].isComplete;
+        this.loadTasks();
+    }
+    deleteTask(event, taskIndex){
+        event.preventDefault();
+        this.tasks.splice(taskIndex, 1);
+        this.loadTasks();
+    }
+    addTaskClick(){
+        let target = document.getElementById('addTask');
+        this.addTask(target.value);
+        target.value = ""
+    }
+    addTask(task) {
+        let newTask = {
+            task,
+            isComplete: false
+        };
+        let parentDiv = document.getElementById('addTask').parentElement;
+        if(task === '') {
+            parentDiv.classList.add('has-error');
+        } else {
+            parentDiv.classList.remove('has-error');
+            this.tasks.push(newTask);
+            this.loadTasks();
+        }
+    }
+    addEventListeners(){
+        document.getElementById('addTask').addEventListener('keypress', 
+        event => {
+            if(event.keyCode === 13) {
+                this.addTask(event.target.value);
+                event.target.value = '';
+            }
+        });
+    }
+
 }
 
 let toDo;
-window.onload = () => { toDo = new ToDoList() }
+window.onload = () => {toDo = new ToDoList()}
 
